87cbb50a5d42f099c4d1dcd76286259a46641b1b
hussienokasha <hussien415532@gmail.com>
2024-06-22 10:08:02 +0300

    "Updated labs-demo and test-demo components with new HTML structure and styling, refactored about-us and contact components, and removed unnecessary code."

--- src/app/Features/Components/test-demo/test-demo.component.ts
+++ src/app/Features/Components/test-demo/test-demo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Test } from 'src/app/Core/Models/test';
+import { TestService } from 'src/app/Core/Services/test.service';
 
 @Component({
   selector: 'app-test-demo',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./test-demo.component.scss']
 })
 export class TestDemoComponent {
+  tests: Test[] = [];
+  constructor(private _testService: TestService) {}
+  ngOnInit(): void {
+    this.getTests();
+  }
 
+  getTests() {
+    this._testService.getAllTests().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.tests = data; // Assign the entire array to the 'lab' variable
+        console.log(this.tests);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
